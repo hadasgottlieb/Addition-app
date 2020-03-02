836daf3d381af06e17917938326998b54a2649f5
Hadas Gottlieb <hadasgottlieb194@gmail.com>
2020-03-01 23:40:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//This function adds text to the output element, which is the two inputs added together. Only if two inputs exist.
+
+function addition() {
+    console.log(`addition function ran.`);
+        $("#equals").on("click", function() {
+            let sum = parseInt($("#number1").val()) + parseInt($("#number2").val());
+            $("#answer").text(`${sum}`);
+        });
+    
+    
+}
+
+function refresh() {
+    console.log(`refresh function ran.`)
+    $("#refresh").on("click", function() {
+        $("#number1").val() = 0;
+        $("#number2").val() = 0;
+    });
+}
+
+function allPage() {
+    addition();
+    refresh();
+}
+
+$(allPage);
